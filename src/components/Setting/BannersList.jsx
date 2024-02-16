a2f73de39772685f2785cb0b33bfffe9f1bf7e80
NikhilRoyal0 <159770607+NikhilRoyal0@users.noreply.github.com>
2024-02-16 17:34:42 +0530

    update

--- src/components/Setting/BannersList.jsx
+++ src/components/Setting/BannersList.jsx
@@ -10,51 +10,30 @@ const Banners = [
   {
     img: user1,
     title: "Exciting Banners in the Tech World!",
-    subtitle:
-      "A groundbreaking technology is set to revolutionize the industry. Experts predict major changes and advancements in the coming months.",
-    btncolor: "error",
   },
   {
     img: user2,
     title: "Innovation Alert: New Discoveries in Science",
-    subtitle:
-      "Scientists have made a remarkable breakthrough in a critical area of research. The implications for the future are enormous.",
-    btncolor: "warning",
   },
   {
     img: user3,
     title: "Global Event: Unveiling of a Remarkable Project",
-    subtitle:
-      "World leaders gather to announce a collaborative effort that promises to address pressing global challenges. Anticipation is high for the project's impact.",
-    btncolor: "primary",
   },
   {
     img: user1,
     title: "Entertainment Buzz: Star-Studded Film Premiere",
-    subtitle:
-      "Celebrities from around the world grace the red carpet for the premiere of the highly anticipated blockbuster film. Fans are eager to see the movie's debut.",
-    btncolor: "success",
   },
   {
     img: user2,
     title: "Health Breakthrough: New Treatment Shows Promise",
-    subtitle:
-      "Researchers unveil a breakthrough treatment that shows promising results in tackling a widespread health issue. The medical community is optimistic about its potential impact.",
-    btncolor: "info",
   },
   {
     img: user3,
     title: "Finance Update: Market Surges to Record High",
-    subtitle:
-      "Financial markets experience a significant surge, reaching record highs. Analysts attribute the positive trend to various economic factors.",
-    btncolor: "error",
   },
   {
     img: user1,
     title: "Sports Highlight: Historic Victory in Championship",
-    subtitle:
-      "In a thrilling match, the underdog team secures a historic victory in the championship. Fans celebrate the unexpected triumph.",
-    btncolor: "warning",
   },
 ];
 
