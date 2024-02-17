4c2f73d735741f7a5288d80e5c8fbcee7e4b1dc4
NikhilRoyal0 <159770607+NikhilRoyal0@users.noreply.github.com>
2024-02-17 16:47:19 +0530

    update

--- src/components/Tasks/TaskList.jsx
+++ src/components/Tasks/TaskList.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
-import { Card, CardContent, Table, TableHead, TableRow, TableCell, TableBody, Checkbox, Chip, Avatar, Button } from '@mui/material';
+import { Card, Typography, CardContent, Table, TableHead, TableRow, TableCell, TableBody, Checkbox, Chip, Avatar, Button, Grid } from '@mui/material';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import { useSelector } from 'react-redux';
 import { selectTaskData } from '../../app/TaskSlice';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function TaskList() {
   const [isTableVisible, setTableVisible] = useState(true);
-
-  const taskData = useSelector(selectTaskData)
-
+  const taskData = useSelector(selectTaskData);
 
   const toggleTableVisibility = () => {
     setTableVisible(!isTableVisible);
@@ -18,56 +17,70 @@ export default function TaskList() {
   return (
     <Card>
       <CardContent>
-        <div style={{ overflow: 'hidden', borderRadius: '15px' }}>
-          <Table style={{ border: '2px solid #ddd' }}>
-            <TableHead style={{ backgroundColor: '#ADD8E6' }}>
-              <TableRow>
-                <TableCell>
-                  <Button onClick={toggleTableVisibility} sx={{ marginLeft: -1 }}>
-                    {isTableVisible ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                  </Button>
-                </TableCell>
-                <TableCell>Tasks List</TableCell>
-                <TableCell>Action Required</TableCell>
-                <TableCell>Schedule</TableCell>
-                <TableCell>Priority</TableCell>
-                <TableCell>Status</TableCell>
-              </TableRow>
-            </TableHead>
-            {isTableVisible && (
-              <TableBody>
-                {taskData.map((task) => (
-                  <TableRow key={task.id}>
-                    <TableCell>
-                      <Checkbox />
-                    </TableCell>
-                    <TableCell>
-                      {task.title}
-                    </TableCell>
-                    <TableCell>
-                      <div style={{ display: 'flex', alignItems: 'center' }}>
-                        <Avatar src={task.avatar} alt={task.name} sx={{ marginRight: 1 }} />
-                        {task.name}
-                      </div>
-                    </TableCell>
-                    <TableCell>{task.date}</TableCell>
-                    <TableCell>
-                      {task.priority === 'Low' && <Chip label="Low" color="default" />}
-                      {task.priority === 'Medium' && <Chip label="Medium" color="primary" />}
-                      {task.priority === 'High' && <Chip label="High" color="secondary" />}
-                      {task.priority === 'Critical' && <Chip label="Critical" color="error" />}
-                    </TableCell>
-                    <TableCell>
-                      {task.status === 'Not Started' && <Chip label="Not Started" color="default" />}
-                      {task.status === 'In Progress' && <Chip label="In Progress" color="info" />}
-                      {task.status === 'Completed' && <Chip label="Completed" color="success" />}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            )}
-          </Table>
-        </div>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Table style={{ border: '2px solid #ddd' }}>
+              <TableHead style={{ backgroundColor: '#ADD8E6' }}>
+                <TableRow>
+                  <TableCell>
+                    <Button onClick={toggleTableVisibility} sx={{ marginLeft: -1 }}>
+                      {isTableVisible ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </Button>
+                  </TableCell>
+                  <TableCell>Tasks List</TableCell>
+                  <TableCell>Action Required</TableCell>
+                  <TableCell>Schedule</TableCell>
+                  <TableCell>Priority</TableCell>
+                  <TableCell>Status</TableCell>
+                </TableRow>
+              </TableHead>
+              {isTableVisible && (
+                <TableBody>
+                  {taskData.map((task) => (
+                    <TableRow key={task.id}>
+                      <TableCell>
+                        <Checkbox />
+                      </TableCell>
+                      <TableCell>
+                        {task.title}
+                      </TableCell>
+                      <TableCell>
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                          <Avatar src={task.avatar} alt={task.name} sx={{ marginRight: 1 }} />
+                          {task.name}
+                        </div>
+                      </TableCell>
+                      <TableCell>{task.date}</TableCell>
+                      <TableCell>
+                        {task.priority === 'Low' && <Chip label="Low" color="default" />}
+                        {task.priority === 'Medium' && <Chip label="Medium" color="primary" />}
+                        {task.priority === 'High' && <Chip label="High" color="secondary" />}
+                        {task.priority === 'Critical' && <Chip label="Critical" color="error" />}
+                      </TableCell>
+                      <TableCell>
+                        {task.status === 'Not Started' && <Chip label="Not Started" color="default" />}
+                        {task.status === 'In Progress' && <Chip label="In Progress" color="info" />}
+                        {task.status === 'Completed' && <Chip label="Completed" color="success" />}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              )}
+            </Table>
+          </Grid>
+
+          <Grid item xs={12} sx={{ textAlign: 'left' }}>
+            <Button
+              sx={{
+                mt: "20px",
+              }}
+              color="primary"
+            >
+              <AddIcon />
+              <Typography sx={{ ml: 1 }}>New Task</Typography>
+            </Button>
+          </Grid>
+        </Grid>
       </CardContent>
     </Card>
   );
--- src/layouts/Logo/LogoIcon.jsx
+++ src/layouts/Logo/LogoIcon.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import logoicn from "../../assets/images/logo-dark.svg";
+import logoicn from "../../assets/images/logo-img.svg";
+
 const LogoIcon = (props) => {
-  return <img alt="Logo" src={logoicn} {...props} />;
+  return <img alt="Logo" src={logoicn} style={{ maxWidth: '200px', height: 'auto', display:'inline-block' }} {...props} />;
 };
 
 export default LogoIcon;
