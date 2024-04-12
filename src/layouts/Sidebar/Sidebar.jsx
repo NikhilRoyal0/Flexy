e531b9b3bc65c7a0061d22d66d792bece8941226
NikhilRoyal0 <159770607+NikhilRoyal0@users.noreply.github.com>
2024-04-12 17:04:10 +0530

    update auth

--- src/layouts/Sidebar/Sidebar.jsx
+++ src/layouts/Sidebar/Sidebar.jsx
@@ -1,181 +1,171 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useLocation } from "react-router";
-import { Link, NavLink } from "react-router-dom";
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import React, { useState, useEffect } from "react";
 import {
+  Card,
+  CardContent,
+  Button,
+  Typography,
+  TextField,
   Box,
-  Drawer,
-  useMediaQuery,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  IconButton
+  FormControlLabel,
+  Checkbox,
+  Link,
+  ThemeProvider,
+  Hidden,
 } from "@mui/material";
-import { SidebarWidth } from "../../assets/global/Theme-variable";
-import LogoIcon from "../Logo/LogoIcon";
+import { createTheme } from "@mui/material/styles";
+import loginimg from "../../assets/images/backgrounds/login-bg.png";
+import { baseTheme } from "../../assets/global/Theme-variable";
+import { useNavigate } from "react-router-dom";
+import { login, isAuthenticated } from "../../utils/auth"; // Import isAuthenticated function
 
-import { fetchMenuItems } from "../../app/menuSlice";
+const theme = createTheme({
+  typography: {
+    fontFamily: "Poppins, sans-serif",
+  },
+});
 
-const Sidebar = (props) => {
-  const dispatch = useDispatch();
-  const { pathname } = useLocation();
-  const pathDirect = pathname;
-  const lgUp = useMediaQuery((theme) => theme.breakpoints.up("lg"));
-
-  useEffect(() => {
-    dispatch(fetchMenuItems());
-  }, [dispatch]);
-
-  const menuItems = useSelector((state) => state.menu.menuItems);
-
-  const [open, setOpen] = React.useState(true);
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
-    setOpen(true);
-  }, [pathDirect]);
-
-  const handleClick = (index, hasSubItems) => {
-    if (lgUp) {
-      props.onSidebarClose();
+    if (isAuthenticated()) {
+      navigate("/");
     }
+  }, []);
 
-    if (!hasSubItems) {
-      props.onSidebarClose();
-    } else if (open === index) {
-      setOpen((prevOpen) => !prevOpen);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const isAuthenticated = login(email, password);
+    if (isAuthenticated) {
+      navigate("/");
     } else {
-      setOpen(index);
+      console.log("Authentication failed. Please check your credentials.");
     }
   };
 
-  const SidebarContent = (
-    <Box sx={{ p: 3, height: "calc(100vh - 40px)" }}>
-      <Link to="/">
-        <Box sx={{ display: "flex", alignItems: "Center" }}>
-          <LogoIcon />
-        </Box>
-      </Link>
-
-      <Box>
-        <List
-          sx={{
-            mt: 0,
-          }}
-        >
-          {menuItems.map((item, index) => (
-            <React.Fragment key={item.id}>
-              <List component="li" disablePadding>
-                <ListItem
-                  key={item.id}
-                  onClick={() => handleClick(index, !!item.subItems)}
-                  button
-                  component={NavLink}
-                  to={item.href}
-                  selected={pathDirect === item.href}
-                  sx={{
-                    mb: 1,
-                    ...(pathDirect === item.href && {
-                      color: "white",
-                      backgroundColor: (theme) =>
-                        `${theme.palette.primary.main}!important`,
-                    }),
-                  }}
-                >
-                  <ListItemIcon
-                    sx={{
-                      ...(pathDirect === item.href && { color: "white" }),
-                    }}
-                  >
-                    <item.icon width="20" height="20" />
-                  </ListItemIcon>
-                  <ListItemText sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                    {item.title}
-                    {item.subItems && (
-                      <IconButton
-                        sx={{ position: 'absolute', right: 20, color: 'inherit', p: 0 }}
-                      >
-                        {open === index ? (
-                          <KeyboardArrowUpIcon />
-                        ) : (
-                          <KeyboardArrowDownIcon />
-                        )}
-                      </IconButton>
-                    )}
-                  </ListItemText>
-                </ListItem>
-              </List>
-
-              {item.subItems && open === index && (
-                <List component="ul" disablePadding sx={{ paddingLeft: 2 }}>
-                  {item.subItems.map((subItem) => (
-                    <ListItem
-                      key={subItem.id}
-                      onClick={() => handleClick(index, false)}
-                      button
-                      component={NavLink}
-                      to={subItem.href}
-                      selected={pathDirect === subItem.href}
-                      sx={{
-                        mb: 1,
-                        ...(pathDirect === subItem.href && {
-                          color: "white",
-                          backgroundColor: (theme) =>
-                            `${theme.palette.primary.main}!important`,
-                        }),
-                      }}
-                    >
-                      <ListItemIcon
-                        sx={{
-                          ...(pathDirect === subItem.href && { color: "white" }),
-                        }}
-                      >
-                        <subItem.icon width="20" height="20" />
-                      </ListItemIcon>
-                      <ListItemText>{subItem.title}</ListItemText>
-                    </ListItem>
-                  ))}
-                </List>
-              )}
-            </React.Fragment>
-          ))}
-        </List>
-      </Box>
-    </Box>
-  );
-  if (lgUp) {
-    return (
-      <Drawer
-        anchor="left"
-        open={props.isSidebarOpen}
-        variant="persistent"
-        PaperProps={{
-          sx: {
-            width: SidebarWidth,
-          },
+  return (
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#f0f0f0",
+          minHeight: "100vh",
         }}
       >
-        {SidebarContent}
-      </Drawer>
-    );
-  }
-  return (
-    <Drawer
-      anchor="left"
-      open={props.isMobileSidebarOpen}
-      onClose={props.onSidebarClose}
-      PaperProps={{
-        sx: {
-          width: SidebarWidth,
-        },
-      }}
-      variant="temporary"
-    >
-      {SidebarContent}
-    </Drawer>
-  );
-};
+        <Card
+          sx={{
+            width: 800,
+            backgroundColor: baseTheme.palette.background.paper,
+            color: baseTheme.palette.text.primary,
+            borderRadius: baseTheme.shape.borderRadius,
+            overflowX: "auto",
+            overflowY: "auto",
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            gap: "20px",
+            "@media (max-width: 960px)": {
+              gridTemplateColumns: "1fr",
+              width: "90%",
+            },
+          }}
+        >
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <Box sx={{ p: 2 }}>
+                <Typography variant="h4" gutterBottom>
+                  Welcome Back!
+                </Typography>
+                <Typography variant="body1" color="textSecondary" gutterBottom>
+                  Welcome Back! Please enter your details
+                </Typography>
 
-export default Sidebar;
+                <br />
+                <Box sx={{ mb: 3 }}>
+                  <Typography variant="body1" gutterBottom>
+                    Email
+                  </Typography>
+                  <TextField
+                    placeholder="Enter your email"
+                    fullWidth
+                    size="small"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} // Update email state
+                  />
+                </Box>
+
+                <Box sx={{ mb: 2 }}>
+                  <Typography variant="body1" gutterBottom>
+                    Password
+                  </Typography>
+                  <TextField
+                    placeholder="Enter your password"
+                    type="password"
+                    fullWidth
+                    size="small"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} // Update password state
+                  />
+                </Box>
+
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    mb: 2,
+                  }}
+                >
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked={false} />}
+                    label={
+                      <Typography variant="body2" sx={{ fontSize: "15px" }}>
+                        Remember me
+                      </Typography>
+                    }
+                  />
+                  <Link href="#" variant="body2">
+                    Forgot Password?
+                  </Link>
+                </Box>
+
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  type="submit"
+                >
+                  Sign in
+                </Button>
+              </Box>
+            </form>
+          </CardContent>
+          <Hidden xsDown smDown mdDown>
+            <Box
+              sx={{
+                flex: 1,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <img
+                src={loginimg}
+                alt="Portrait"
+                style={{
+                  maxWidth: "100%",
+                  maxHeight: "100%",
+                  height: 450,
+                  objectFit: "cover",
+                }}
+              />
+            </Box>
+          </Hidden>
+        </Card>
+      </Box>
+    </ThemeProvider>
+  );
+}
